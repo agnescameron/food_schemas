e7c2442cffc16df5f7504217243617f5805ee8e9
Agnes Cameron <agnesfcameron@gmail.com>
2020-12-23 11:15:15 +0000

    updated graph data entry

--- graph_data_entry.py
+++ graph_data_entry.py
@@ -37,27 +37,27 @@ def insert_data(recipe):
 	recipe_id = 0
 	#add recipe, author and source to the graph
 	tx = graph_db.begin()
-	recipeNode = matcher.match("http://schema.org/Recipe", wasDerivedFromURL=recipe.source).first()
+	recipeNode = matcher.match("http://underlay.org/Recipe", wasDerivedFromURL=recipe.source).first()
 	if recipeNode == None:
-		recipeNode = Node("http://schema.org/Recipe", name=recipe.title, wasDerivedFromURL=recipe.source)
+		recipeNode = Node("http://underlay.org/Recipe", name=recipe.title, wasDerivedFromURL=recipe.source)
 		print('source is', recipe.source)
 
-	author = matcher.match("http://schema.org/Author", name=recipe.author).first()
+	author = matcher.match("http://underlay.org/Author", name=recipe.author).first()
 	if author == None:
-		author = Node("http://schema.org/Author", name=recipe.author)
+		author = Node("http://underlay.org/Author", name=recipe.author)
 		tx.create(author)
 
 	rootURL = re.match(r'http?s:\/\/([a-z]+\.){1,}[a-z]+', recipe.source)[0]
 	print(rootURL)
 
-	source = matcher.match("http://schema.org/Webpage", url=rootURL).first()
+	source = matcher.match("http://underlay.org/Webpage", url=rootURL).first()
 	if source == None:
-		source = Node("http://schema.org/Webpage", url=rootURL)
+		source = Node("http://underlay.org/Webpage", url=rootURL)
 		tx.create(source)
 
 	tx.create(recipeNode)
-	auth_relation = Relationship(recipeNode, "hasAuthor", author)
-	source_relation = Relationship(recipeNode, "hasSource", source)
+	auth_relation = Relationship(recipeNode, "http://underlay.org/hasAuthor", author)
+	source_relation = Relationship(recipeNode, "http://underlay.org/hasSource", source)
 	tx.create(auth_relation)
 	tx.create(source_relation)
 	tx.commit()
@@ -65,37 +65,37 @@ def insert_data(recipe):
 
 	for ingredient in recipe.ingredients:
 		tx = graph_db.begin()
-		ingredient = cleaning.clean_ingredient(ingredient)
-		matched_ingredient = match_label(ingredient, recipe.title)
+		original_ingredient = ingredient
+		cleaned_ingredient = cleaning.clean_ingredient(ingredient)
+		matched_ingredient = match_label(cleaned_ingredient, recipe.title)
 
 		if matched_ingredient:
-			ingredient = matcher.match("http://schema.org/Ingredient", name=matched_ingredient["label"]).first()
+			ingredient = matcher.match("http://underlay.org/Ingredient", matchedFoodONEntity=matched_ingredient["label"]).first()
 			if ingredient == None:
 				meat = False
 				for word in meats:
 					if word in matched_ingredient["label"].lower(): meat = True
-				ingredient = Node("http://schema.org/Ingredient", name=matched_ingredient["label"], containsMeat=meat)
+				print('1', cleaned_ingredient, '2', original_ingredient, '3', matched_ingredient["label"])
+				ingredient = Node("http://underlay.org/Ingredient", hasCleanedName=cleaned_ingredient, matchedFoodONEntity=matched_ingredient["label"], hasEntryInRecipe=original_ingredient, containsMeat=meat)
 				tx.create(ingredient)
-			relation = Relationship(recipeNode, "hasIngredient", ingredient)
+			relation = Relationship(recipeNode, "http://underlay.org/hasIngredient", ingredient)
 			tx.create(relation)
 		tx.commit()
 
 	for tag in recipe.tags:
 		tx = graph_db.begin()
 
-		tagNode = matcher.match("http://schema.org/Tag", name=tag).first()
+		tagNode = matcher.match("http://underlay.org/Tag", name=tag).first()
 		if tagNode == None:
-			tagNode = Node("http://schema.org/Tag", name=tag)
+			tagNode = Node("http://underlay.org/Tag", name=tag)
 			tx.create(tagNode)
-		relation = Relationship(recipeNode, "hasAssociatedTag", tagNode)
+		relation = Relationship(recipeNode, "http://underlay.org/hasAssociatedTag", tagNode)
 		tx.create(relation)
 		tx.commit()
 
 if __name__ == "__main__":
-
-
 	graph_db = Graph(uri, auth=(user, password))
-	# graph_db.delete_all()
+	graph_db.delete_all()
 	matcher = NodeMatcher(graph_db)
 
 	recipes = mod.scrape()
